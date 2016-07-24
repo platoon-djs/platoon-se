acdafd20ab1d0f7d1a00ade44d5866d2d38f5cfa
Linus Wåreus <wareus@kth.se>
2016-07-24 17:38:50 +0200

    Added page "Gå med"

---
+++ app/controllers/JoinController.php
@@ -0,0 +1,23 @@
+<?php
+
+
+/**
+ * JoinController
+ */
+class JoinController extends BaseController
+{
+
+	public function __construct()
+	{
+		parent::__construct();
+
+		$this->data['title'] = 'Gå med';
+		$this->data['slug'] = 'join';
+	}
+
+	public function index()
+	{
+		$this->render('@pages/join.twig');
+	}
+	
+}
--- app/routes.php
+++ app/routes.php
@@ -3,6 +3,7 @@
 $app->get('/', 'IndexController:index')->name('home');
 $app->get('/about', 'AboutController:index')->name('about');
 $app->get('/members', 'MembersController:index')->name('members');
+$app->get('/join', 'JoinController:index')->name('join');
 
 $app->get('/booking', 'BookingController:index')->name('booking');
 $app->get('/booking/place/:query', 'BookingController:place');
